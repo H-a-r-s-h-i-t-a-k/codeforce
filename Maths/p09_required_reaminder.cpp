31e2110854a17dd5600e20ac7777d1c565506010
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-07-19 19:01:27 +0530

    done

---
+++ Maths/p09_required_reaminder.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long t;
+    long long x, y, n;
+    cin >> t;
+    while (t--)
+    {
+        cin >> x >> y >> n;
+        long long ans = 0;
+        ans = n - n % x + y;
+        if (ans <= n)
+        {
+            cout << ans << endl;
+        }
+        else
+        {
+            cout << (n - n % x - (x - y)) << endl;
+        }
+    }
+
+    return 0;
+}

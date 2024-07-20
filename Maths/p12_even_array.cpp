8ce930e18c9918eb797ecd27b6e5426fbb3cc7b5
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-07-20 18:47:59 +0530

    done

--- Maths/p12_even_array.cpp
+++ Maths/p12_even_array.cpp
@@ -1,2 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+void solve()
+{
+    int a = 0, b = 0, n, arr[1000];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        if (arr[i] % 2 != i % 2)
+        {
+            if (arr[i] % 2 == 0)
+                a++;
+            else
+                b++;
+        }
+    }
+    if (b != a)
+        cout << "-1" << endl;
+    else
+        cout << a << endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

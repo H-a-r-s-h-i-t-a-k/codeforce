4da8252977cdd72b0f3f6c84bc873427ced58141
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-07-22 01:35:44 +0530

    done

--- Maths/p17_yet_another_two_integer_problem.cpp
+++ Maths/p17_yet_another_two_integer_problem.cpp
@@ -8,9 +8,24 @@ int main()
     {
         cin >> a >> b;
         int dif = abs(a - b);
-        int res = ceil(dif / 10);
+        int res = ceil(dif / 10.0);
         cout << res << endl;
     }
 
     return 0;
 }
+// 1.
+
+// while (t--)
+// {
+//     cin >> a >> b;
+//     cout << (abs(a - b) + 9) / 10 << endl;
+// }
+// 2.
+// while (t--)
+// {
+//     cin >> a >> b;
+//     int dif = abs(a - b);
+//     int res = ceil(dif / 10.0);
+//     cout << res << endl;
+// }
---
+++ Maths/p18_candies_and_2sisters.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long num;
+        cin >> num;
+        cout << (num - 1) / 2 << endl;
+    }
+    return 0;
+}

9458a351ab5c0219ed74f8f730bc8b5f4d17f004
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-07-24 02:00:49 +0530

    done

--- Maths/p22_domino_piling.cpp
+++ Maths/p22_domino_piling.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    int M, N;
+    float M, N;
     cin >> M >> N;
-    cout << M * N / 2 << endl;
+    cout << floor(M * N * 0.5) << endl;
     return 0;
 }
---
+++ Maths/p23_emotes.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ll m, n, k, x;
+    cin >> n >> m >> k;
+    int m1 = 0, m2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        if (x > m1)
+        {
+
+            m2 = m1;
+            m1 = x;
+        }
+        else if (m2 < x)
+            m2 = x;
+    }
+    ll times = m / (k + 1);
+    ll cost = k * m1 + m2;
+    cost *= times;
+    cost += (m % (k + 1) * m1);
+    cout << cost;
+
+    return 0;
+}

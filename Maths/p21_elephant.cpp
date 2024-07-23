a5a06ede5dd727b15741e7569a8947816730c5b7
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-07-23 23:21:15 +0530

    done

---
+++ Maths/p21_elephant.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num;
+    cin >> num;
+
+    if (num % 5 == 0)
+        cout << num / 5 << endl;
+    else
+        cout << (num / 5) + 1 << endl;
+
+    return 0;
+}

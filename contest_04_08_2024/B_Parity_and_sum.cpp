47391f60c3d4eefa2584e61adacda37a4ec6bc06
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-08-05 18:11:58 +0530

    b done

---
+++ contest_04_08_2024/B_Parity_and_sum.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ll t, n;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        ll ar[n], v, l_odd = -1;
+        vector<ll> odd, even;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v;
+            ar[n] = v;
+
+            if (v % 2 != 0)
+            {
+
+                if (v > l_odd)
+                    l_odd = v;
+            }
+            else
+            {
+                even.push_back(v);
+            }
+        }
+
+        if (even.empty() || l_odd == -1)
+        {
+            cout << 0 << endl;
+            continue;
+        }
+
+        // odd.sort();
+        sort(even.begin(), even.end());
+        int operation = even.size();
+        for (int i = 0; i < even.size(); i++)
+        {
+
+            if (even[i] < l_odd)
+            {
+                l_odd += even[i];
+            }
+            else
+            {
+
+                operation++;
+
+                break;
+            }
+        }
+
+        cout << operation << endl;
+    }
+    return 0;
+}

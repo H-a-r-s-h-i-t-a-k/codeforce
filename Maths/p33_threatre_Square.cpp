ab44b21481955e6916452727739eb5e725354392
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-08-14 18:14:39 +0530

    done

--- Maths/p33_threatre_Square.cpp
+++ Maths/p33_threatre_Square.cpp
@@ -1,1 +1,11 @@
-p
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    unsigned long long n, m, a, ans;
+    cin >> n >> m >> a;
+    ans = ceil((double)m / a) * ceil((double)n / a);
+    cout << ans << endl;
+    return 0;
+}

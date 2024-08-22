15b9d98d061e2ec11bcdd32808db967ecf4b4c0b
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-08-23 00:05:35 +0530

    done

--- Maths/p47_Lucky_number.cpp
+++ Maths/p47_Lucky_number.cpp
@@ -1,34 +1,13 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-
-vector<long long> lucky;
-
-void trY(long long number, int fours, int sevens)
-{
-    if (number > 1e10)
-        return;
-
-    if (fours == sevens)
-        lucky.push_back(number);
-
-    trY(number * 10 + 7, fours, sevens + 1);
-    trY(number * 10 + 4, fours + 1, sevens);
-}
 
 int main()
 {
-    trY(4, 1, 0);
-    trY(7, 0, 1);
 
-    long long n;
+    ll n;
     cin >> n;
-
-    sort(lucky.begin(), lucky.end());
-    cout << lucky[lower_bound(lucky.begin(), lucky.end(), n) - lucky.begin()] << "\n";
+    cout << 2 * ((ll)pow(2, n) - 1);
 
     return 0;
 }

285e4f23615844c33fa288163582fc3745979dc4
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-08-27 15:00:42 +0530

    done

--- contest/q3.cpp
+++ contest/q3.cpp
@@ -1,7 +1,59 @@
-#include<iostream>
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <vector>
 using namespace std;
-int main(){
+typedef long long ll;
 
-    
+string solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    map<char, int> mp;
+    for (auto it : s)
+        mp[it]++;
+
+    vector<pair<int, char>> vec;
+    for (auto it : mp)
+        vec.push_back({it.second, it.first});
+    // sort(vec.end(), vec.begin());
+    sort(vec.begin(), vec.end());
+    reverse(vec.begin(), vec.end());
+
+    string temp = "", ans = "";
+    int sum = 0;
+    for (auto it : vec)
+        temp += it.second;
+
+    int i = vec.size() - 1;
+    while (i >= 0)
+    {
+
+        int req = vec[i].first - sum;
+        for (int j = 0; j < req; j++)
+            ans += temp;
+
+        sum += req;
+        temp.pop_back();
+        i--;
+    }
+
+    return ans;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        cout << solve() << endl;
+    }
+
     return 0;
 }
